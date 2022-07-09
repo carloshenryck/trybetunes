1da8843ad946ac0e2cd6461718639a98612ca5b8
Henryck <ryck302@gmail.com>
2022-07-09 14:24:24 -0300

    requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      isLoading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({
+      user,
+      isLoading: false,
+    });
+  }
+
   render() {
+    const { user, isLoading } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
+        { isLoading
+          ? <Loading />
+          : (
+            <>
+              <div>{user.name}</div>
+              <div>{user.email}</div>
+              <div>{user.description}</div>
+              <img src={ user.image } alt="user profile" data-testid="profile-image" />
+              <Link to="/profile/edit"> Editar perfil </Link>
+            </>
+          )}
       </div>
     );
   }
